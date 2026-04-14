11f9f6286a10c70c865ea2d6f333d51fcad9d244
ToriTalks2much <x68gh5zdjt@privaterelay.appleid.com>
2026-04-14 08:28:23 -0400

    Refactor ticket validation tests to use ticket_validator

--- test_ticket_validator.py
+++ test_ticket_validator.py
@@ -1,17 +1,19 @@
+import ticket_validator
+
 def test_valid_ticket(): 
-  assert validate_ticket("TK674269") == True
+  assert ticket_validator.validate_ticket("TK674269") == True
 
 def test_invalid_ticket(): 
   assert validate_ticket("42") == False
 
 def test_general_ticket():
-  assert get_ticket_tier("TK159841") == "General" 
+  assert ticket_validator.get_ticket_tier("TK159841") == "General" 
 
 def test_VIP_ticket():
-  assert get_ticket_tier("TK459841") == "VIP" 
+  assert ticket_validator.get_ticket_tier("TK459841") == "VIP" 
 
 def test_Platinum_ticket():
-  assert get_ticket_tier("TK759841") == "Platinum" 
+  assert ticket_validator.get_ticket_tier("TK759841") == "Platinum" 
 
 def test_too_much_discount():
   assert calculate_total(10, 1.1) == ValueError
